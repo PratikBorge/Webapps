b8edd95a3eb804ee14ef1102734c5da3d66a18b0
Pratik <pratikborge37@gmail.com>
2024-02-27 15:02:04 +0530

    script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,6 @@
 pipeline{
     agent any
     stages{
-        stage("pull from git"){
-            steps{
-                git 'https://github.com/PratikBorge/Webapps.git'
-            }
-        }
         stage("install nginx"){
             steps{
             sudo apt install nginx -y
